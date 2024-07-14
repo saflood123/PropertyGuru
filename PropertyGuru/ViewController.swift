ea9f79f5ca8e349818130f0e6148a4f69c484b16
saflood123 <stuartflood@gmail.com>
2024-07-14 10:11:23 +0100

    Initial Commit

---
+++ PropertyGuru/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PropertyGuru
+//
+//  Created by stuart flood on 14/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
